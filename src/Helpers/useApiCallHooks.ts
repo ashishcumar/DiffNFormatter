5a0107863246f17d24ffb0b4e92610e58e39f627
Ashish Kumar <ashish.kumar@zopper.com>
2025-05-27 14:40:12 +0530

    added firebase

--- src/Helpers/useApiCallHooks.ts
+++ src/Helpers/useApiCallHooks.ts
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-// const baseUrl = "https://diffnformatter-be.onrender.com/api";
-const baseUrl = "http://localhost:8000/api";
+const baseUrl = "https://diffnformatter-be.onrender.com/api";
+// const baseUrl = "http://localhost:8000/api";
 const endpoints = {
   diff: "/diff/",
   format: "/format/",
